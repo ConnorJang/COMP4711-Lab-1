2dc3fd3912a23291d7baa2c210132a33de8796f0
Connor <connorjang20@gmail.com>
2017-09-07 13:37:01 -0700

    Committing index file and readme changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # COMP4711-Lab-1
 Thursday, September 7, 2017
+Small change to readme file
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Connor
+ * Date: 9/7/2017
+ * Time: 1:31 PM
+ */ ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php echo "Hello out there... We're on the air... It's hockey night tonight" ?>
+    </body>
+</html>
